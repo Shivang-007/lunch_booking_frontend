ccd91260858c4f68847683ffdd0c4f03b2047fe1
Shivang Rathod <shivangrathod007@gmail.com>
2022-05-06 14:39:19 +0530

    button enable-disable performed

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 
-function addGuests() {
+
+
+
+document. getElementById("delete_request").style.display = "none";
+document. getElementById("arrive_lunch").style.display = "none";
+
+function openGuest() {
     document.querySelector('.popup').style.display = "flex";
 };
 
-document.querySelector(".close").addEventListener("click", function () {
-    document.querySelector(".popup").style.display = "none";
+var e1 = document.querySelector(".close");
+
+if(e1){
+    e1.addEventListener("click", function () {
+        document.getElementById('popup').style.display = "none";     
 });
+}
+
+
 
